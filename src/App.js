87a281d788ebcffdd132f322d5d5d4b831ae6f63
JPham8010 <jpham8010@gmail.com>
2023-05-22 22:37:48 -0500

    render components and style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import About from "./components/about";
+import Contact from "./components/contact";
+import Home from "./components/home";
+import Navbar from "./components/navbar";
+import Skills from "./components/skills";
+import Works from "./components/works";
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Works />
+      <Contact />
     </div>
   );
 }
-
 export default App;
